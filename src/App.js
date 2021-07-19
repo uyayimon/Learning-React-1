cbbd7a147c0b6c5da9c19d5c7481f2aceb61456b
uyayimon <mmjhmk@gmail.com>
2021-07-19 14:34:07 +0900

    styled-componentsによるスタイルの適用

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import styled from "styled-components";
 import { useEffect, useState } from "react";
 import { List } from "./List";
 import { Form } from "./Form";
 import { getLanguages } from "./const/languages";
+
+const Header = styled.header`
+  display: flex;
+  justify-content: space-between;
+  padding: 24px 64px 0;
+  border-bottom: 1px solid #e0e0e0;
+`;
+
+const HeaderUl = styled.ul`
+  display: flex;
+  margin: 0;
+  padding: 0;
+`;
+
+const HeaderLi = styled.li`
+  list-style: none;
+  padding: 4px 12px;
+  cursor: pointer;
+  border-bottom: ${(props) => (props.focused ? "2px solid #F44336" : "none")};
+`;
 
 function App() {
   const [tab, setTab] = useState("list");
@@ -15,23 +36,26 @@ function App() {
   const fetchLanguages = async () => {
     const languages = await getLanguages();
     setLangs(languages);
-  }
+  };
 
   const addLang = (lang) => {
     setLangs([...langs, lang]);
-    setTab('list');
-  }
+    setTab("list");
+  };
 
   return (
     <div>
-      <header>
-        <ul>
-          <li onClick={() => setTab("list")}>リスト</li>
-          <li onClick={() => setTab("form")}>フォーム</li>
-        </ul>
-      </header>
-      <hr />
-      {tab === "list" ? <List langs={langs}/> : <Form onAddLang={addLang}></Form>}
+      <Header>
+        <HeaderUl>
+          <HeaderLi focused={ tab ==="list"} onClick={() => setTab("list")}>リスト</HeaderLi>
+          <HeaderLi focused={ tab ==="form"} onClick={() => setTab("form")}>フォーム</HeaderLi>
+        </HeaderUl>
+      </Header>
+      {tab === "list" ? (
+        <List langs={langs} />
+      ) : (
+        <Form onAddLang={addLang}></Form>
+      )}
     </div>
   );
 }
--- src/List.js
+++ src/List.js
@@ -1,19 +1,22 @@
-import { useEffect } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  padding: 12px 64px;
+`;
+const ListItem = styled.div`
+  padding: 8px 16px;
+
+  &:nth-child(n+2) {
+    border-top: 1px solid #d9dbde;
+  }
+`;
 
 export const List = ({ langs }) => {
-  useEffect(() => {
-    console.log("List.js:useEffect");
-
-    return () => {
-      console.log("List.js: useEffect: unmount");
-    };
-  });
-
   return (
-    <div>
+    <Container>
       {langs.map((lang, index) => {
-        return <div key={index}>{lang}</div>;
+        return <ListItem key={index}>{lang}</ListItem>;
       })}
-    </div>
+    </Container>
   );
 };
